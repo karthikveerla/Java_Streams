c3d0971bfb83d1a4c2c7161b6963ee935a873db1
karthik <karthik@karthik>
2020-07-24 22:07:53 +0530

    java8 lambdas and streams commit

---
+++ java8_Lambdas_Streams/src/java8_Lambdas_Streams/FilterStrings.java
@@ -0,0 +1,21 @@
+package java8_Lambdas_Streams;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.*;
+public class FilterStrings 
+{
+	public static List<String> filter(List<String> list) 
+	{
+		return list.stream().filter(s -> s.startsWith("a") && s.length()==3).collect(Collectors.toList());
+	}
+	public static void main(String args[]) 
+	{
+		List<String> strings = new ArrayList<String>();
+		 strings.add("abc"); 
+	        strings.add("aaa"); 
+	        strings.add("bbb");
+	        strings.add("abcd");
+	        List<String> s = filter(strings); 
+	        System.out.println(s);	
+	}
+}
---
+++ java8_Lambdas_Streams/src/java8_Lambdas_Streams/Refer.java
@@ -0,0 +1,39 @@
+package java8_Lambdas_Streams;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+public class Refer {
+	public static void main(String[] args) {
+        List<String> a = Arrays.asList("14561","12221","1222344321","malayalam","010","12212221");
+        List<String> palindrome = Refer.findPal(a,(str) -> Refer.isPalindrome((String) str));
+        System.out.println("Palindromes are " + palindrome);
+    }
+    public static boolean isPalindrome(String str) 
+    {
+    	StringBuffer sb =new StringBuffer();
+    	for(int i = sb.length()-1; i >= 0 ; i--) 
+    	{
+    	sb = sb.append(str.charAt(i));
+    	}
+    	if(str.equalsIgnoreCase(sb.toString())) 
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
+        } 
+    public static List<String> findPal(List<String> list, Predicate<String> predicate) { 
+    	List<String> sorted = new ArrayList<String>(); 
+    	list
+    	.stream()
+    	.filter((i) -> (predicate.test((String) i))).forEach((i) -> {
+            sorted.add((String) i);
+        });
+        return sorted;
+ 
+    }
+    
+}
