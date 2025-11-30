5a7a51119d3b2ca8052771a7148b383e3c1b1815
Karthik Veerla <karthikveerla28@gmail.com>
2025-11-29 23:33:32 -0500

    dept with high avg salary

--- java8-lambdas-streams/src/main/java/com/example/Intermediate/DeptHighAvgSalary.java
+++ java8-lambdas-streams/src/main/java/com/example/Intermediate/DeptHighAvgSalary.java
@@ -1,15 +1,13 @@
 package com.example.Intermediate;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.example.dtos.Employee;
 
-public class DeptHaveMoreThanTwoEmp {
+public class DeptHighAvgSalary {
     public static void main(String args[]){
         List<Employee> emps = Arrays.asList(new Employee(1,"Alex","HR",3000),
                                             new Employee(2,"Bob","ITS",2000),
@@ -22,16 +20,13 @@ public class DeptHaveMoreThanTwoEmp {
                                             new Employee(10,"Jacob","HR",2500),
                                             new Employee(4,"David","ITS",1000));
 
-    List<String> res =
-        emps.stream()
+    Map.Entry<String,Double> res =  emps.stream()
             .collect(Collectors.groupingBy(
                 Employee::getDept,
-                Collectors.counting()
-            ))
-            .entrySet().stream()
-                            .filter(e-> e.getValue() > 2)
-                            .map(Map.Entry::getKey)
-                            .toList();
+                Collectors.averagingDouble(Employee::getSalary)
+            )).entrySet().stream()
+                            .max(Map.Entry.comparingByValue())
+                            .orElseThrow();
 
     System.out.println(res);
     }
