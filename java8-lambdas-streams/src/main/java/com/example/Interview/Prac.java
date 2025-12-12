258cb3d1613ce45ad7015a4834d9a87fbc1605cd
Karthik Veerla <karthikveerla28@gmail.com>
2025-12-12 14:16:49 -0500

    hackerrank practice

---
+++ java8-lambdas-streams/src/main/java/com/example/Interview/Prac.java
@@ -0,0 +1,15 @@
+package com.example.Interview;
+
+public class Prac {
+    public static void main(String[] args) {
+        
+        int a =010;
+        /* Here is a comment **** */
+        // /* This is also a comment /* More comments */ */
+        /* This is also a comment // More comments */
+        // /* This is a // // comment */
+        int b = 07;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
